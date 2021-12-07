db5cc30cf48a887a7df5469e8ffaec2e886e66af
fernada <gaspar.fernanda25@gmail.com>
2021-12-07 07:47:53 -0300

    add

---
+++ siteP/resources/views/portifolio/create.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UFT-8">
+    <meta name='viewport' content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>cadastastro de portifolio</title>
+</head>
+<body>
+        <form action="">
+            <label for="">Nome</label> <br />
+            <input type="text" nome="nome"> <br />
+            <label for="">quantidade</label> <br />
+            <input type="text" nome="quantidade"> <br />
+        </form>
+</body>
+</html>
